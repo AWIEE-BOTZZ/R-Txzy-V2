1a5ae2d8399e251308b3814d1a47bd4f878168e3
AWIEE-BOTZZ <96273425+AWIEE-BOTZZ@users.noreply.github.com>
2022-02-24 07:42:43 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -4,8 +4,8 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), `
 ┌〔 Donasi • Emoney 〕
 ├ GoPay: tanya ownerbot
-├ Dana:0895367208200 [Yulianti]
-├ Pulsa:085852311805 [Awiebot]
+├ Dana: 0895367208200 [Yulianti]
+├ Pulsa: 085852311805 [Awiebot]
 └────
 `.trim(), watermark, 'Menu', '.menu', m)
 handler.help = ['donasi']
